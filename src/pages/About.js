1c5c162ee13fb61782e2bb61b0786856898ee8cf
Ogunrinde Eunice Modupe <31671705+PenielShell@users.noreply.github.com>
2022-12-26 09:39:53 +0100

    Inital commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quos, quibusdam ab maxime reprehenderit, qui, necessitatibus alias cupiditate quasi id libero distinctio eum aliquam sint earum veniam atque minima aperiam!
+      </p>
+    </section>
+  )
+}
+
+export default About
